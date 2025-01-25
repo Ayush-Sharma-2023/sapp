ff3dc6054726c786b7ae9c72549f886217a0539d
RoamingTrooper <altafraza7206@gmail.com>
2025-01-25 12:06:35 +0530

    Harsh commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
- <h1>Hello world</h1>
+ {/* <h1>Hello world</h1>
  <h1> Harshvardhan Here</h1>
  <h1>Test</h1>
  <h1>Test 2</h1>
@@ -11,7 +11,8 @@ export default function Home() {
   <h2>Ayush here</h2>
   <h1>Harsh here</h1>
   <h3>altaf second testing</h3>
-  <h2>#rfdfsdfdsf</h2>
+  <h2>#rfdfsdfdsf</h2> */}
+  <h3>All testing done</h3>
 
     
     {/* /*
