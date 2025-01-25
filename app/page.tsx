1586f4d524aff4f3d46f625591ed69d5060882ed
Ayush-Sharma-VIH <ayush@VidyaInternaHub.onmicrosoft.com>
2025-01-25 12:42:21 +0530

    Ayush commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,13 @@
 import { Navbar } from "@/components/Navbar";
+// import {Home} from "@/components/Home";
+import Main from "./Main/page";
 export default function Home() {
   return (
     <>
- {/* <h1>Hello world</h1>
- <h1> Harshvardhan Here</h1>
- <h1>Test</h1>
- <h1>Test 2</h1>
-  <h1>Test 3</h1>
-  <h3>Altaf here 1st change</h3>
-  <h2>Ayush here</h2>
-  <h1>Harsh here</h1>
-  <h3>altaf second testing</h3>
-  <h2>#rfdfsdfdsf</h2> */}
-  {/* <h3>All testing done</h3> */}
-  <Navbar />
-    {/* /*
-    git add . 
-    git commit -m "Harsh commit"
-    git push
-    */}
 
+  <Navbar />
+  <Main />
+ 
  
  </>
   );
