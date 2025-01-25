74e7abf1e8dbf53222b0c8559e5328d621adfeba
Harshvs2003 <sinha.hv2003@gmail.com>
2025-01-25 11:28:07 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 
-
 export default function Home() {
   return (
- <>
+    <>
  <h1>Hello world</h1>
+ <h1> Harshvardhan Here</h1>
+
+    
+    {/* /*
+    git add . 
+    git commit -m "Initial commit"
+    git push
+    */ */}
 
  
  </>
